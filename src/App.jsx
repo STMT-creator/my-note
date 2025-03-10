ad2bb1b36b8a9570566f09f10ad6ec4bc3cb3568
stmt <elahep113@naver.com>
2025-03-10 09:06:11 +0900

    명칭 변경

--- src/App.jsx
+++ src/App.jsx
@@ -28,14 +28,14 @@ function Body({ onCreate }) {
           setCount(count + 1);
         }}
       >
-        <fieldset className="memo-container d-flex flex-column mx-auto">
+        <fieldset className="note-container d-flex flex-column mx-auto">
           <legend>메모 노트</legend>
           <input
             type="text"
             className="my-1"
             name="title"
             id="title"
-            placeholder="memo title"
+            placeholder="note title"
             value={title}
             onChange={e => setTitle(e.target.value)}
           />
@@ -45,15 +45,15 @@ function Body({ onCreate }) {
             id="body"
             cols="30"
             rows="5"
-            placeholder="memo text"
+            placeholder="note text"
             value={body}
             onChange={e => setBody(e.target.value)}
           ></textarea>
           <div className="d-flex gap-1 justify-end">
-            <button type="button" className="memo-del">
+            <button type="button" className="note-del">
               취소
             </button>
-            <button type="submit" className="memo-create">
+            <button type="submit" className="note-create">
               기록
             </button>
           </div>
@@ -62,24 +62,24 @@ function Body({ onCreate }) {
     </>
   );
 }
-function Footer({ memos }) {
-  console.log(memos);
+function Footer({ notes }) {
+  console.log(notes);
   return (
     <>
-      <ul className="memos-area d-flex">
-        {memos.map(memo => {
+      <ul className="notes-area d-flex">
+        {notes.map(note => {
           return (
-            <li key={memo.id} className="memo-box posi-relative">
-              <div className="memo-title d-flex items-center justify-between posi-relative">
-                <h3>{memo.title}</h3>
+            <li key={note.id} className="note-box posi-relative">
+              <div className="note-title d-flex items-center justify-between posi-relative">
+                <h3>{note.title}</h3>
                 <img
                   src="../images/edit-icon.png"
                   alt="edit-icon"
                   className="edit-title-icon posi-absolute"
                 />
               </div>
-              <div className="memo-body">
-                <p>{memo.body}</p>
+              <div className="note-body">
+                <p>{note.body}</p>
               </div>
               <img
                 src="../images/edit-icon.png"
@@ -95,19 +95,19 @@ function Footer({ memos }) {
 }
 
 function App() {
-  const [memos, setMemos] = useState([]);
+  const [notes, setnotes] = useState([]);
   const [id, setId] = useState(1);
   function onCreate(title, body, count) {
     // console.log('제목에', title, '내용에', body);
-    const newMemo = { id: count, title: title, body: body };
-    setMemos([...memos, newMemo]);
+    const newNote = { id: count, title: title, body: body };
+    setnotes([...notes, newNote]);
     setId(count);
   }
   return (
     <>
       <Header Header={Header} />
       <Body Body={Body} onCreate={onCreate} />
-      <Footer memos={memos} />
+      <Footer notes={notes} />
     </>
   );
 }
